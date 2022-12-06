368d261b6372365ee67c6f05038e5db3dac96d63
Jerry-Do <dangkhoado2@gmail.com>
2022-12-05 21:18:09 -0500

    intial commit

--- server.js
+++ server.js
@@ -1,3 +1,14 @@
+/*********************************************************************************
+* WEB322 – Assignment 06
+* I declare that this assignment is my own work in accordance with Seneca Academic Policy. No part of this
+* assignment has been copied manually or electronically from any other source (including web sites) or 
+* distributed to other students.
+* 
+* Name: Jerry Do Student ID: 116797218 Date: 05-12-2022
+*
+* Online (Cyclic) Link: https://dull-gray-kingfisher-tie.cyclic.app
+*
+********************************************************************************/ 
 const express = require("express");
 const path = require("path");
 const exphbs = require("express-handlebars");
